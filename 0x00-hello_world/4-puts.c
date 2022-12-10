e04d0c9494f252a77ab36e1ada2800a194346264
Casopia-Beki <bereketkassaye@gmail.com>
2022-12-10 10:35:01 -0800

    Hello, printf

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,11 @@
 #include <stdio.h>
 
 /**
- * main -Print a line of code using puts
+ *main -Entry point
  *
- * Retun: 0
+ *Retun: Always 0 (Success)
  */
+
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
