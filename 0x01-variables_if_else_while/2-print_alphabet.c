6d35838677879e43d4b64b60ba3bcfd18c98bd4b
Casopia-Beki <bereketkassaye@gmail.com>
2022-12-11 07:31:47 -0800

    Insomnia

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - returns alphabets
+ *
+ * Return: Always 0 (Success)
+ *
+ */
+int main(void)
+{
+	char lc;
+
+	for (lc = 'a'; lc <= 'z'; lc++)
+
+	{
+		putchar(lc);
+	}
+
+	putchar('\n');
+	Return(0);
+}
