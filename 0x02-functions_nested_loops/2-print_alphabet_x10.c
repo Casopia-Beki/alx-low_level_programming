c98954c1eb6a1afeebe3c6e0c2992db43b55f102
Casopia-Beki <bereketkassaye@gmail.com>
2022-12-14 08:30:30 -0800

    10 x alphabet

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,7 +1,7 @@
 #include "main.ih"
 
 /**
- * print_alphabet_x10 - prints alpha 10 times
+ * print_alphabet_x10 - print alphabet 10 times
  *
  * Return: Always 0 (Success)
  *
@@ -9,14 +9,11 @@
 void print_alphabet_x10(void)
 {
 
-	char c;
-	int i = 0;
+	char letter;
 
-	while (i <= 9)
-	{
-		for (c = 'a'; c <= 'z'; c++)
-			_putchar(c);
+	for letter = 'a'; letter <= 'z'; letter++
+			_putchar(letter);
 
 	_putchar('\n');
-	}
+
 }
