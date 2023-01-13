7396157badf267bcaf5ab9f27495786595388731
Casopia-Beki <bereketkassaye@gmail.com>
2023-01-13 13:51:08 -0800

    _calloc

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -28,8 +28,8 @@ void *_calloc(unsigned int nmemb, unsigned int size)
 	char *ptr;
 
 	if (nmemb == 0 || size == 0)
-		return (NULL)
-	ptr = malloc(size * memb);
+		return (NULL);
+	ptr = malloc(size * nmemb);
 	if (ptr == NULL)
 		return (NULL);
 
