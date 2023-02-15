18a3085106587ad936a8bd7d7182e22a0179f51d
Casopia-Beki <bereketkassaye@gmail.com>
2023-02-15 09:05:40 -0800

    Print list

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -1,29 +1,22 @@
-#include "main.h"
-#include <stdlib.h>
+#include "lists.h"
 
 /**
- * read_textfile- Read text file print to STDOUT.
- * @filename: text file being read
- * @letters: number of letters to be read
- * Return: w- actual number of bytes read and printed
- *        0 when function fails or filename is NULL.
+ * print_listint - prints all the elements of a linked list
+ * @h: linked list of type listint_t to print
+ *
+ * Return: number of nodes
  */
-ssize_t read_textfile(const char *filename, size_t letters)
+size_t print_listint(const listint_t *h)
 {
-	char *buf;
-	ssize_t fd;
-	ssize_t w;
-	ssize_t t;
+ size_t num = 0;
 
-	fd = open(filename, O_RDONLY);
-	if (fd == -1)
-		return (0);
-	buf = malloc(sizeof(char) * letters);
-	t = read(fd, buf, letters);
-	w = write(STDOUT_FILENO, buf, t);
+ while (h)
+ {
+ printf("%d\n", h->n);
+ num++;
+ h = h->next;
+ }
 
-	free(buf);
-	close(fd);
-	return (w);
+ return (num);
 }
 
